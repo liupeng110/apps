a90a0061b0e8b84c197f27e4b4d2e893462e0ff4
liupeng110 <717219917@qq.com>
2017-03-01 16:02:58 +0800

    0.0

--- app/src/main/java/com/andlp/apps/activity/Activity_Main.java
+++ app/src/main/java/com/andlp/apps/activity/Activity_Main.java
@@ -1,13 +1,17 @@
 package com.andlp.apps.activity;
 
-import android.os.Bundle; 
+import android.os.Bundle;
 import com.andlp.apps.R;
 
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_main)
 public class Activity_Main extends Activity_Base {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
     }
 }
---
+++ app/src/main/java/com/andlp/apps/activity/Activity_Welcome.java
@@ -0,0 +1,23 @@
+package com.andlp.apps.activity;
+
+import android.os.Bundle;
+
+import com.andlp.apps.R;
+
+import org.xutils.view.annotation.ContentView;
+
+/**
+ * 717219917@qq.com  2017/3/1 16:01
+ */
+@ContentView(R.layout.activity_welcome)
+public class Activity_Welcome extends Activity_Base {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+
+
+}
