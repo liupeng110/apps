75e33ce71b62cedd5d512289b4c466d324b39919
liupeng110 <717219917@qq.com>
2017-03-01 15:24:28 +0800

    Initial commit

---
+++ app/src/main/java/com/andlp/apps/App.java
@@ -0,0 +1,13 @@
+package com.andlp.apps;
+
+import com.andlib.lp.AndApp;
+/**
+ * 717219917@qq.com  2017/3/1 13:52
+ */
+public class App extends AndApp {
+
+
+
+
+
+}
---
+++ app/src/main/java/com/andlp/apps/activity/Activity_Main.java
@@ -0,0 +1,15 @@
+package com.andlp.apps.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.andlp.apps.R;
+
+public class Activity_Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
